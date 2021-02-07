3e3f6cc948112a74e7a27dccf4433245cfed7173
rojon <rjrojon@gmail.com>
2021-02-07 06:46:53 +0600

    onchange display item fixed

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-const division = document.getElementById('division');
-const district = document.getElementById('district');
-const upazilla = document.getElementById('upazilla');
+const division = document.getElementById("division");
+const district = document.getElementById("district");
+const upazilla = document.getElementById("upazilla");
 
 const displayOptions = (area, item) => {
-    let option = document.createElement('option');
-    option.value = item.toLowerCase();
-    option.innerText = item;
-    area.appendChild(option);
-}
+  let option = document.createElement("option");
+  option.value = item.toLowerCase();
+  option.innerText = item;
+  area.appendChild(option);
+};
 
-fetch('https://bdapis.herokuapp.com/api/v1.0/divisions')
-    .then(res => res.json())
-    .then(res => res.data)
-    .then(res => res.map(div => div.division))
-    .then(res => res.map(item => displayOptions(division, item)));
+fetch("https://bdapis.herokuapp.com/api/v1.0/divisions")
+  .then((res) => res.json())
+  .then((res) => res.data)
+  .then((res) => res.map((div) => div.division))
+  .then((res) => res.map((item) => displayOptions(division, item)));
 
-division.addEventListener('change', e => {
+division.addEventListener("change", (e) => {
+  e.preventDefault();
+  district.innerHTML = `<option value='' selected>-- Select District --</option>`;
+  upazilla.innerHTML = `<option value='' selected>-- Select Upazilla --</option>`;
+  if (division.value != "0") {
+    var fetchData = fetch(
+      `https://bdapis.herokuapp.com/api/v1.0/division/${division.value}`
+    )
+      .then((res) => res.json())
+      .then((res) => res.data);
+
+    fetchData
+      .then((res) => res.map((div) => div.district))
+      .then((res) => res.map((item) => displayOptions(district, item)));
+  }
+
+  district.addEventListener("change", (e) => {
+      upazilla.innerHTML = `<option value='' selected>-- Select Upazilla --</option>`;
     e.preventDefault();
-    if (division.value != '0') {
-        var fetchData = fetch(`https://bdapis.herokuapp.com/api/v1.0/division/${division.value}`)
-            .then(res => res.json())
-            .then(res => res.data);
-
-        fetchData
-            .then(res => res.map(div => div.district))
-            .then(res => res.map(item => displayOptions(district, item)))
-        // .then(res => console.log(res))
+    if (district.value != "0") {
+      fetchData
+        .then((res) => res.find((upazilla) => upazilla._id == district.value))
+        .then((res) =>
+          res.upazilla.map(item => displayOptions(upazilla,item))
+        );
     }
-
-    district.addEventListener('change', e => {
-        e.preventDefault();
-        if (district.value != '0') {
-            fetchData
-                .then(res => res.find(upazilla => upazilla._id == district.value))
-                .then(res => res.upazilla.map(item => displayOptions(upazilla, item)))
-            // .then(res => res.map(item => displayOptions(upazilla, item)))
-        }
-    });
+  });
 });
 
-
-
-document.getElementById('adress').addEventListener('submit', e=>{
-    e.preventDefault();
-    const address = `<div class="card">
+document.getElementById("adress").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const address = `<div class="card">
                     <h2>You are from:</h2>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item text-capitalize">${division.value}</li>
-                        <li class="list-group-item text-capitalize">${district.value}</li>
-                        <li class="list-group-item text-capitalize">${upazilla.value}</li>
+                        <li class="list-group-item text-capitalize"><strong>Division:</strong> ${division.value}</li>
+                        <li class="list-group-item text-capitalize"><strong>District:</strong> ${district.value}</li>
+                        <li class="list-group-item text-capitalize"><strong>Upazilla:</strong> ${upazilla.value}</li>
                     </ul>
                 </div>`;
 
-    document.getElementById('address-area').innerHTML = address;
-})
+  document.getElementById("address-area").innerHTML = address;
+});
